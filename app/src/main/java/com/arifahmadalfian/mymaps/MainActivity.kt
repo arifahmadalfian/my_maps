de9cf6fd4d1a46d55caba21b0c526381348a1796
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-30 19:47:25 +0700

    Menambahkan marker

--- app/src/main/java/com/arifahmadalfian/mymaps/MainActivity.kt
+++ app/src/main/java/com/arifahmadalfian/mymaps/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.arifahmadalfian.mymaps
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ICON_ID = "ICON_ID"
+    }
 
     private lateinit var mapView: MapView
     private lateinit var btnNavigation: Button
     private lateinit var mapboxMap: MapboxMap
+    private lateinit var symbolManager: SymbolManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +34,36 @@ class MainActivity : AppCompatActivity() {
         mapView.getMapAsync { mapboxMap ->
             this.mapboxMap = mapboxMap
             mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+                symbolManager = SymbolManager(mapView, mapboxMap, style)
+                symbolManager.iconAllowOverlap = true
 
+                style.addImage(
+                    ICON_ID,
+                    BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default)
+                )
+
+                showDicodingSpace()
             }
         }
+    }
+
+    private fun showDicodingSpace() {
+        val dicodingSpace = LatLng(-6.8957643, 107.6338462)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(dicodingSpace.latitude, dicodingSpace.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+                .withTextField("Dicoding Space")
+                .withTextHaloColor("rgba(255, 255, 255, 100")
+                .withTextHaloWidth(5.0f)
+                .withTextAnchor("top")
+                .withTextOffset(arrayOf(0f, 1.5f))
+                .withDraggable(true)
+        )
+
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 8.0))
     }
 
     override fun onStart() {
