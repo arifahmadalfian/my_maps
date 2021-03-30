655e9b41f67f4fc128de68e4063b7703b4027544
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-30 18:55:52 +0700

    Initial commit

---
+++ app/src/main/java/com/arifahmadalfian/mymaps/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.arifahmadalfian.mymaps
+
+import android.app.Application
+import com.mapbox.mapboxsdk.Mapbox
+
+open class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
+    }
+}
